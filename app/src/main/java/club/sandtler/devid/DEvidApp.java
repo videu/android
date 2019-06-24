626fdc61c2624797965a5efeabdf7b96fc59d753
Felix Kopp <sandtler@sandtler.club>
2019-06-24 18:05:29 +0200

    Add main app class

---
+++ app/src/main/java/club/sandtler/devid/DEvidApp.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019 Felix Kopp <sandtler@sandtler.club>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package club.sandtler.devid;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
+/**
+ * The main application class.
+ * This is only used for making the application context globally accessible.
+ */
+public class DEvidApp extends Application {
+
+    /** The application context. */
+    /* This is ok because it gets null-referenced upon termination. */
+    @SuppressLint("StaticFieldLeak")
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DEvidApp.context = getApplicationContext();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        DEvidApp.context = null;
+    }
+
+    /**
+     * Return the application context.
+     *
+     * @return The application context.
+     */
+    @Nullable
+    public static Context getAppContext() {
+        return DEvidApp.context;
+    }
+
+}
